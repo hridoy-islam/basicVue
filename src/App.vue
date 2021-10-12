4534b81d257d71e4af22bb3062593cfbb268a396
Hridoy Islam <hridoy4t@gmail.com>
2021-10-12 19:38:20 +0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <h1>Hello !! - {{ name }}</h1>
+    <h2>Greetings !! - {{ greet }}</h2>
+    <div v-text="email"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      name: "Md. Ridoy ",
+      greet: "Salam",
+      email: "hridoy4t@gmail.com",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
